554f7b4826e024bf87e5d73bf90f9aa1b9ea2e05
Atila Neves <atila.neves@gmail.com>
2026-05-27 09:47:26 +0200

    Parse function ranges with libdparse

--- source/dcrap/source_parser.d
+++ source/dcrap/source_parser.d
@@ -11,16 +11,81 @@ public struct FunctionRange
     public LineRange lineRange;
 }
 
-public FunctionRange[] parseFunctionRanges(in string filePath) @safe pure
+public FunctionRange[] parseFunctionRanges(in string filePath)
 {
-    return [
-        FunctionRange(
-            qualifiedName: "sample.covered",
-            filePath: filePath,
-            lineRange: LineRange(
-                firstLine: 3,
-                lastLine: 6,
-            ),
-        ),
-    ];
+    import std.file : readText;
+
+    return filePath.readText.parseFunctionRanges(filePath);
+}
+
+private FunctionRange[] parseFunctionRanges(in string source, in string filePath)
+{
+    import dparse.ast : ASTVisitor, FunctionDeclaration, Module;
+    import dparse.lexer : LexerConfig, StringCache, getTokensForParser;
+    import dparse.parser : ParserConfig, parseModule;
+    import dparse.rollback_allocator : RollbackAllocator;
+
+    final class FunctionRangeVisitor : ASTVisitor
+    {
+        public alias visit = ASTVisitor.visit;
+
+        private string filePath;
+        private string moduleName;
+        private string source;
+        public FunctionRange[] functionRanges;
+
+        public this(in string filePath, in string moduleName, in string source)
+        {
+            this.filePath = filePath;
+            this.moduleName = moduleName;
+            this.source = source;
+        }
+
+        public override void visit(const FunctionDeclaration declaration)
+        {
+            import std.conv : text;
+
+            functionRanges ~= FunctionRange(
+                qualifiedName: text(moduleName, ".", declaration.name.text),
+                filePath: filePath,
+                lineRange: LineRange(
+                    firstLine: declaration.name.line,
+                    lastLine: source.lineAt(declaration.functionBody.endLocation),
+                ),
+            );
+        }
+    }
+
+    auto cache = StringCache(StringCache.defaultBucketCount);
+    LexerConfig lexerConfig;
+    auto tokens = getTokensForParser(source, lexerConfig, &cache);
+    RollbackAllocator allocator;
+    const Module parsedModule = ParserConfig(tokens, filePath, &allocator)
+        .parseModule;
+
+    auto visitor = new FunctionRangeVisitor(filePath, parsedModule.moduleName,
+        source);
+    parsedModule.accept(visitor);
+    return visitor.functionRanges;
+}
+
+private string moduleName(in imported!"dparse.ast".Module parsedModule)
+{
+    import std.algorithm : map;
+    import std.array : join;
+
+    if (parsedModule.moduleDeclaration is null) {
+        return null;
+    }
+
+    return parsedModule.moduleDeclaration.moduleName.identifiers
+        .map!(identifier => identifier.text)
+        .join(".");
+}
+
+private size_t lineAt(in string source, in size_t byteOffset) @safe pure
+{
+    import std.algorithm : count;
+
+    return source[0 .. byteOffset].count('\n') + 1;
 }
--- tests/ut/source_parser.d
+++ tests/ut/source_parser.d
@@ -31,3 +31,37 @@ unittest
         ),
     ];
 }
+
+@("source parser derives module name, function name, and range from source")
+unittest
+{
+    import dcrap.source_parser : FunctionRange, parseFunctionRanges;
+    import std.path : absolutePath;
+    import std.string : outdent, stripRight;
+
+    immutable sandbox = Sandbox();
+    sandbox.writeFile("source/other.d", q{module audit.sample;
+
+        int changed(int value)
+        {
+            if (value > 0) {
+                return value;
+            }
+
+            return 0;
+        }
+    }.outdent.stripRight);
+
+    const filePath = sandbox.inSandboxPath("source/other.d").absolutePath;
+
+    filePath.parseFunctionRanges.should == [
+        FunctionRange(
+            qualifiedName: "audit.sample.changed",
+            filePath: filePath,
+            lineRange: LineRange(
+                firstLine: 3,
+                lastLine: 10,
+            ),
+        ),
+    ];
+}
